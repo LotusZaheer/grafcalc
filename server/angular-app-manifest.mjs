22f4d8ce0210392a8f574434cf277f660e151d09
Andrés Uribe <andres.uribe@orusxpert.com>
2025-06-16 19:47:55 -0500

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 20552, hash: '41dc1a112ae118236fb868c4868493135e676249f81a5e8de3d80fa0974dbf68', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1024, hash: '181694b95c6e3f316bb90a0eafbda87be0a3d52ccbf3d55581e70052e5fdea0e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 32554, hash: '6865c02acc8bab04f54f0f968b69421ede2e624e8856184c232a4db6638c1e44', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 20552, hash: 'a59a7d7cdc47998f5bf91a2359d4e4fc0c4ffa39214469132a748f37c00ed6ce', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1024, hash: 'e0debeee2b8f62390c474c551a91045209b5748be5173cde7764951420cc8b0e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 34216, hash: 'c2b72ccbb4e1a0bf6c91d9c1f8d03f17989ad7b1f18fc57d525cd6ed3d8ccdcc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-COVNJEGA.css': {size: 21630, hash: 'l1qjRFS/9bI', text: () => import('./assets-chunks/styles-COVNJEGA_css.mjs').then(m => m.default)}
   },
 };
